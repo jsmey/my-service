ddeb2b617a61f7d562f1b5e2cf7bef96b506b608
John Smey <jsmey@godaddy.com>
2019-02-01 17:11:48 -0700

    initial commit

---
+++ custom-serverless-plugins/sc-serverless-plugin/AwsCompileServiceCatalog.js
@@ -0,0 +1,174 @@
+'use strict';
+
+const BbPromise = require('bluebird');
+const path = require('path');
+const crypto = require('crypto');
+const fs = require('fs');
+
+// eslint-disable-next-line no-template-curly-in-string
+const cfProvisionedProductTemplate = {
+  Type: 'AWS::ServiceCatalog::CloudFormationProvisionedProduct',
+  Properties: {
+    ProvisioningParameters: [
+      { Key: 'BucketName', Value: 'ServerlessDeploymentBucket' },
+      { Key: 'BucketKey', Value: 'S3Key' },
+      { Key: 'FunctionName', Value: 'FunctionName' },
+      { Key: 'FunctionStage', Value: 'test' },
+      { Key: 'FunctionHandler', Value: 'Handler' },
+      { Key: 'FunctionRuntime', Value: 'Runtime' },
+      { Key: 'FunctionMemorySize', Value: 'MemorySize' },
+      { Key: 'FunctionTimeout', Value: 'Timeout' },
+      { Key: 'LambdaVersionSHA256', Value: 'CodeSha256' },
+    ],
+    ProvisioningArtifactName: 'ProvisioningArtifactName',
+    ProductId: 'ProductId',
+    ProvisionedProductName: { 'Fn::Sub': 'provisionServerless-${FunctionName}' },
+  },
+};
+
+class AwsCompileServiceCatalog {
+  constructor(serverless, options) {
+    this.serverless = serverless;
+
+    this.options = options;
+    const servicePath = this.serverless.config.servicePath || '';
+    this.packagePath = this.serverless.service.package.path
+      || path.join(servicePath || '.', '.serverless');
+    this.provider = this.serverless.getProvider('aws');
+
+    // key off the ServiceCatalog Product ID
+    if ('scProductId' in this.serverless.service.provider) {
+      this.newFunction = cfProvisionedProductTemplate;
+      this.hooks = {
+        'before:package:finalize': () => BbPromise.bind(this)
+          .then(this.compileFunctions),
+      };
+
+      this.serverless.cli.log('AwsCompileServiceCatalog');
+      // clear out any other aws plugins
+      this.serverless.pluginManager.hooks['package:compileEvents'].length = 0;
+      this.serverless.pluginManager.hooks['package:compileFunctions'].length = 0;
+      this.serverless.pluginManager.hooks['package:setupProviderConfiguration'].length = 0;
+    }
+  }
+
+  compileFunctions() {
+    const allFunctions = this.serverless.service.getAllFunctions();
+    return BbPromise.each(
+      allFunctions,
+      functionName => this.compileFunction(functionName),
+    );
+  }
+
+  compileFunction(functionName) {
+    const functionObject = this.serverless.service.getFunction(functionName);
+    functionObject.package = functionObject.package || {};
+
+    const serviceArtifactFileName = this.provider.naming.getServiceArtifactName();
+    const functionArtifactFileName = this.provider.naming.getFunctionArtifactName(functionName);
+
+    let artifactFilePath = functionObject.package.artifact
+      || this.serverless.service.package.artifact;
+    if (!artifactFilePath
+      || (this.serverless.service.artifact && !functionObject.package.artifact)) {
+      let artifactFileName = serviceArtifactFileName;
+      if (this.serverless.service.package.individually || functionObject.package.individually) {
+        artifactFileName = functionArtifactFileName;
+      }
+
+      artifactFilePath = path.join(this.serverless.config.servicePath,
+        '.serverless', artifactFileName);
+    }
+    if (this.serverless.service.provider.deploymentBucket) {
+      this.setProvisioningParamValue('BucketName', this.serverless.service.provider.deploymentBucket);
+    } else {
+      const errorMessage = 'Missing provider.deploymentBucket parameter.'
+        + ' Please make sure you provide a deployment bucket parameter. SC Provisioned Product cannot create an S3 Bucket.'
+        + ' Please check the docs for more info';
+      return BbPromise.reject(new this.serverless.classes.Error(errorMessage));
+    }
+
+    const s3Folder = this.serverless.service.package.artifactDirectoryName;
+    const s3FileName = artifactFilePath.split(path.sep).pop();
+    this.setProvisioningParamValue('BucketKey', `${s3Folder}/${s3FileName}`);
+
+    if (!functionObject.handler) {
+      const errorMessage = `Missing "handler" property in function "${functionName}".`
+        + ' Please make sure you point to the correct lambda handler.'
+        + ' For example: handler.hello.'
+        + ' Please check the docs for more info';
+      return BbPromise.reject(new this.serverless.classes.Error(errorMessage));
+    }
+
+    const MemorySize = Number(functionObject.memorySize)
+      || Number(this.serverless.service.provider.memorySize)
+      || 1024;
+    const Timeout = Number(functionObject.timeout)
+      || Number(this.serverless.service.provider.timeout)
+      || 6;
+    const Runtime = functionObject.runtime
+      || this.serverless.service.provider.runtime
+      || 'nodejs4.3';
+
+    this.setProvisioningParamValue('FunctionHandler', functionObject.handler);
+    this.setProvisioningParamValue('FunctionName', functionObject.name);
+    this.setProvisioningParamValue('FunctionMemorySize', MemorySize);
+    this.setProvisioningParamValue('FunctionTimeout', Timeout);
+    this.setProvisioningParamValue('FunctionRuntime', Runtime);
+    this.setProvisioningParamValue('FunctionStage', this.provider.getStage());
+    this.newFunction.Properties.ProvisioningArtifactName = this.serverless.service.provider.scProductVersion;
+    this.newFunction.Properties.ProductId = this.serverless.service.provider.scProductId;
+    this.newFunction.Properties.ProvisionedProductName = `provisionSC-${functionObject.name}`;
+
+    // publish these properties to the platform
+    this.serverless.service.functions[functionName].memory = MemorySize;
+    this.serverless.service.functions[functionName].timeout = Timeout;
+    this.serverless.service.functions[functionName].runtime = Runtime;
+
+    if (functionObject.tags || this.serverless.service.provider.tags) {
+      const tags = Object.assign(
+        {},
+        this.serverless.service.provider.tags,
+        functionObject.tags,
+      );
+      this.newFunction.Properties.Tags = Object.keys(tags).map(key => ({ Key: key, Value: tags[key] }));
+    }
+
+    const fileHash = crypto.createHash('sha256');
+    fileHash.setEncoding('base64');
+
+    return BbPromise.fromCallback((cb) => {
+      const readStream = fs.createReadStream(artifactFilePath);
+      readStream.on('data', (chunk) => {
+        fileHash.write(chunk);
+      })
+        .on('end', cb)
+        .on('error', cb);
+    })
+      .then(() => {
+        // Finalize hashes
+        fileHash.end();
+        const fileDigest = fileHash.read();
+        this.setProvisioningParamValue('LambdaVersionSHA256', fileDigest);
+
+        const functionLogicalId = `${this.provider.naming.getNormalizedFunctionName(functionName)}SCProvisionedProduct`;
+        this.serverless.service.provider.compiledCloudFormationTemplate.Resources[functionLogicalId] = this.newFunction;
+        this.serverless.service.provider.compiledCloudFormationTemplate.Outputs.ProvisionedProductID = {
+          Description: 'Provisioned product ID',
+          Value: { Ref: functionLogicalId },
+        };
+        return BbPromise.resolve();
+      });
+  }
+
+  setProvisioningParamValue(key, value) {
+    const index = this.newFunction.Properties.ProvisioningParameters.findIndex(kv => kv.Key === key);
+    if (index === -1) {
+      console.error(`object with Key=${key} not found in ProvisioningParameters!`);
+      return;
+    }
+    this.newFunction.Properties.ProvisioningParameters[index].Value = value;
+  }
+}
+
+module.exports = AwsCompileServiceCatalog;
